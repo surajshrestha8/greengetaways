f6b233c85fc160d1c959c79a1cd16bed94c9ddc3
surajshrestha8 <surajshre348@gmail.com>
2026-02-18 19:50:31 +0545

    update brand fonts

--- greengetaways/src/app/(frontend)/layout.tsx
+++ greengetaways/src/app/(frontend)/layout.tsx
@@ -48,6 +48,19 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
 
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://api.fontshare.com" />
+        <link
+          href="https://api.fontshare.com/v2/css?f[]=atyp-text@400,600&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <Header tours={tours} logoUrl={logoUrl} />
         <main>{children}</main>
