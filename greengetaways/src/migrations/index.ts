95920d026b356ddeda830c734992db7cc6c8ab0c
surajshrestha-3ptrades <suraj.shrestha@3ptrades.com>
2026-06-21 18:13:31 +0545

    add migration

--- greengetaways/src/migrations/index.ts
+++ greengetaways/src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20260508_170000_booking_date_availability from './20260508_170000_booking_date_availability'
+import * as migration_20260508_170000_booking_date_availability from './20260508_170000_booking_date_availability';
+import * as migration_20260621_122816 from './20260621_122816';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20260508_170000_booking_date_availability.down,
     name: '20260508_170000_booking_date_availability',
   },
-]
+  {
+    up: migration_20260621_122816.up,
+    down: migration_20260621_122816.down,
+    name: '20260621_122816'
+  },
+];
